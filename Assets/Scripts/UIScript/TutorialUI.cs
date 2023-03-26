5228055dd48ee2f9ad7391f89563b2775613e8a1
Batuhan Efe Arıalp <batuhanarialp@hotmail.com>
2023-03-26 11:20:23 +0300

    TUTORIAL UI

--- Assets/Scripts/UIScript/TutorialUI.cs
+++ Assets/Scripts/UIScript/TutorialUI.cs
@@ -6,10 +6,9 @@ using System.Collections.Generic;
 public class TutorialUI : MonoBehaviour
 {
     private Text tutorialText;
-    private int line = 0;
     private bool isShot, isStab, isJump;
     private bool processing = false;
-    private List<string> str = new List<string>();
+    private readonly List<string> str = new();
     Color col;
 
 
@@ -45,40 +44,76 @@ public class TutorialUI : MonoBehaviour
 
     private void TextChanger()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
             isShot = true;
         }
-        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             isJump = true;
         }
-        if (Input.GetMouseButtonDown(1) && !isStab)
+        if (Input.GetMouseButtonDown(1))
         {
             isStab = true;
+        }
+
+
+        if (!processing)
+        {
+            if (!isShot)
+            {
+                StartCoroutine(TextShowTime());
+            }
+            else if (!isJump)
+            {
+                StartCoroutine(TextShowTime());
+            }
+            else if (!isStab)
+            {
+                StartCoroutine(TextShowTime());
+            }
+        }       
+        if (isShot)
+        {
+            StartCoroutine(TextTimerFirst());
         }
     }
 
 
     private IEnumerator TextTimerFirst()
     {
-        tutorialText.text = str[0];
-        FadeIn();
-        processing = true;
-        // yield break
-        yield return new WaitForSecondsRealtime(25f);
-        if (isShot)
+        if (tutorialText.text == str[0] && isShot)
         {
             FadeOut();
-        }        
+        }
+        else if (tutorialText.text == str[1] && isJump)
+        {
+            FadeOut();
+        }
+        else if (tutorialText.text == str[2] && isStab)
+        {
+            FadeOut();
+        }
+        else
+        {
+            yield break;
+        }
+        yield return new WaitForSecondsRealtime(2f);
+        processing = false;
     } 
 
+    private IEnumerator TextShowTime()
+    {     
+        FadeIn();
+        print("upper side");
+        yield return new WaitForSecondsRealtime(2f);
+        processing = true;
+        print("lower side");
+    }
 
 
     void Update()
     {
         TextChanger();
-        TextTimerFirst();
     }
 }
